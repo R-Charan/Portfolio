3e11ea6a4124f06b16557ea34e07b91ffee815df
R Charan Bhardhwaj <rcb.rcharan@gmail.com>
2025-08-10 21:17:19 +0530

    Converted to Hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import ProjectDetail from './ProjectDetail.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router basename="/Portfolio/">
+    <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/projects/:id" element={<ProjectDetail />} />
